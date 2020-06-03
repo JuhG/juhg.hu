9a94a557f395c29b29b0fd0a5fe871a1ae3451d9
JuhG <juhg24@gmail.com>
2020-06-03 09:01:53 +0200

    🐈 replace cat gif api

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,26 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
+const getCatUrl = () => {
+  return fetch(
+    'https://api.giphy.com/v1/gifs/random?api_key=SSJvg27oBJtfwU228k9C2hXe77kfPibz&tag=cat'
+  )
+    .then(resp => resp.json())
+    .then(resp => resp.data.image_url)
+}
+
 const NotFoundPage = () => {
-  const gif = useRef()
+  const [url, setUrl] = useState(null)
+
+  useEffect(() => {
+    if ('undefined' === typeof window) {
+      return
+    }
+
+    getCatUrl().then(url => setUrl(url))
+  }, [])
+
   return (
     <Layout>
       <SEO title="404" description="Page not found" />
@@ -13,20 +30,12 @@ const NotFoundPage = () => {
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
         <p>To ease the pain, here's a random cat gif:</p>
 
-        <img
-          ref={gif}
-          className="mt-16 mx-auto"
-          src="https://cataas.com/cat/gif"
-          alt="CAT"
-        />
+        {url && <img className="mt-16 mx-auto" src={url} alt="CAT" />}
 
         <div className="flex justify-center">
           <button
             className="dd-button mt-16"
-            onClick={() => {
-              gif.current.src =
-                'https://cataas.com/cat/gif?' + new Date().getTime()
-            }}
+            onClick={async () => setUrl(await getCatUrl())}
           >
             Load another
           </button>
