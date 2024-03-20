a1e50ec62c946743eab4ca2d08b107be8646ed60
Gabor Juhasz <juhg24@gmail.com>
2024-03-20 09:52:54 +0100

    init

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Personal space of Gábor Juhász";
-export const SITE_DESCRIPTION = "An always curious full-stack web developer focused on React and UX 💻";
