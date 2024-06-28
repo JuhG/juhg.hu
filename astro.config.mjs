a149394b27c550c6c9369b9c2c5fbd2f09b708c4
Gabor Juhasz <juhg24@gmail.com>
2024-06-28 22:38:47 +0200

    use static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), react()],
-  output: "server",
+  output: "static",
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
