25a49b2799b45ad21566b928fccb20b093e26d8c
Gabor Juhasz <juhg24@gmail.com>
2024-10-28 08:27:37 +0100

    something new

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 export const Footer = () => {
   return (
     <div className="flex items-center justify-between border-t-2 p-6">
-      <div>
-        <a href="/" className="text-gray-500 hover:text-gray-700">
+      <a href="/" className="text-gray-500 hover:text-gray-700">
+        <div className="flex items-center gap-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -17,8 +17,9 @@ export const Footer = () => {
               d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
             />
           </svg>
-        </a>
-      </div>
+          <span>Gabor Juhasz</span>
+        </div>
+      </a>
       <div className="flex gap-2">
         <a
           href="https://github.com/JuhG"
